8c3e589da2407a9973d5c75516ca7266ad7077ae
Alireza Shafi'i <16956112+alirezashafii@users.noreply.github.com>
2022-06-06 23:16:21 +0430

    create module

---
+++ javascript/products.js
@@ -0,0 +1,32 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Bicycle",
+    price: 766,
+    imageUrl: "./assets/images/cycle.jpg",
+  },
+  {
+    id: 2,
+    title: "E-Scooter",
+    price: 989,
+    imageUrl: "./assets/images/e-scooter.jpg",
+  },
+  {
+    id: 3,
+    title: "Bike",
+    price: 2640,
+    imageUrl: "./assets/images/bike.jpg",
+  },
+  {
+    id: 4,
+    title: "X Car",
+    price: 94000,
+    imageUrl: "./assets/images/x-tron-car.jpg",
+  },
+  {
+    id: 5,
+    title: "Future Plant",
+    price: 199,
+    imageUrl: "./assets/images/future-plant.jpg",
+  },
+];
