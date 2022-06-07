9623b0f9c8c8dd0863411c12acc06f077ff43b89
Alireza Shafi'i <16956112+alirezashafii@users.noreply.github.com>
2022-06-07 13:11:45 +0430

    update js

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,9 @@ const cartIcon = document.querySelector(".cartIcon");
 const cartBtn = document.querySelector(".cartBtn");
 const backDrop = document.querySelector(".backdrop");
 const confirmBtn = document.querySelector(".cart-item-confirm");
+const productsParent = document.querySelector(".main-child");
+
+import { productsData } from "./products.js";
 
 toggler.addEventListener("click", () => {
   document.body.classList.toggle("dark");
@@ -28,3 +31,42 @@ function closeFunc() {
   cart.style.transform = "translateY(-100vh)";
   cart.style.opacity = "0";
 }
+
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+
+class Ui {
+  displayProducts(items) {
+    let result = "";
+    items.forEach((item) => {
+      result += `<div class="item">
+                  <img src=${item.imageUrl} alt="" loading="lazy">
+                  <section class="description">
+                      <p class="title">${item.title}</p>
+                      <p class="price">$${item.price}</p>
+                  </section>
+                  <button class="add-to-cart" data-id=${item.id}>
+                    add to cart
+                  </button>
+                </div>`;
+      productsParent.innerHTML = result;
+    });
+  }
+}
+
+class Storage {
+  static saveProducts(datas) {
+    localStorage.setItem("products", JSON.stringify(datas));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProducts();
+  const ui = new Ui();
+  ui.displayProducts(productsData);
+  Storage.saveProducts(productsData);
+});
