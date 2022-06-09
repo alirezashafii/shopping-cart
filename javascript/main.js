456971817b5ed189ba7f7419fd7401eb8c373c1e
Alireza Shafi'i <16956112+alirezashafii@users.noreply.github.com>
2022-06-09 12:39:56 +0430

    added scroll tracker

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,27 @@ const backDrop = document.querySelector(".backdrop");
 const confirmBtn = document.querySelector(".cart-item-confirm");
 const productsParent = document.querySelector(".main-child");
 
+// Scroll Tracker
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+  source: document.scrollingElement,
+  orientation: "block",
+  scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+  {
+    transform: ["scaleX(0)", "scaleX(1)"],
+  },
+  {
+    duration: 1,
+    timeline: scrollTrackingTimeline,
+  }
+);
+
 import { productsData } from "./products.js";
 
 let cart = [];
